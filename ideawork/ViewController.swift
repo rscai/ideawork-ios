426fbe6e46a3e78568bcfd000d7b61b30fb2dc61
kkppccdd <kkppccdd@gmail.com>
2015-02-21 13:25:53 +0800

    Initial Commit

---
+++ ideawork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ideawork
+//
+//  Created by 蔡士雷 on 2015/2/21.
+//  Copyright (c) 2015年 Ray Cai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
